fcd53cf79c2936b0ead502a7f5fcfaac899d722d
Fabian Yate <fabianyate31@gmail.com>
2024-02-11 23:12:05 -0500

    add icon

--- src/App.tsx
+++ src/App.tsx
@@ -46,6 +46,21 @@ function App() {
           <div className="flex gap-4">
             <DropdownButton estados={estados} />
             <Button color="blue" className="my-6" onClick={toggleFormVisible}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-plus mr-2"
+              >
+                <path d="M5 12h14" />
+                <path d="M12 5v14" />
+              </svg>
               Agregar postulación
             </Button>
           </div>
