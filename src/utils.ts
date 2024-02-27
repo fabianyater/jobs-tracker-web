db15020e25c5f3e814cdb51fab134873bf9d83c2
Fabian Yate <fabianyate31@gmail.com>
2024-02-26 22:28:32 -0500

    Implemented relative time utility function
    Added a new function to calculate the relative time difference between a given date and the current date. This enhances the user experience by returning human-readable time differences like "3 days ago" or "1 minute ago", improving the app's readability and interface friendliness.

--- src/utils.ts
+++ src/utils.ts
@@ -13,6 +13,33 @@ export function formatearFecha(fechaOriginal: string) {
   });
 }
 
+export function calcularDiferenciaFecha(fechaOriginal: string): string {
+  const ahora = new Date();
+  const fecha = new Date(fechaOriginal);
+  const diferenciaEnMilisegundos = ahora.getTime() - fecha.getTime();
+
+  const minutos = Math.floor(diferenciaEnMilisegundos / 60000);
+  const horas = Math.floor(minutos / 60);
+  const dias = Math.floor(horas / 24);
+  const meses = Math.floor(dias / 30);
+  const años = Math.floor(dias / 365);
+
+  if (años > 0) {
+    return `hace ${años} año${años > 1 ? 's' : ''}`;
+  } else if (meses > 0) {
+    return `hace ${meses} mes${meses > 1 ? 'es' : ''}`;
+  } else if (dias > 0) {
+    return `hace ${dias} día${dias > 1 ? 's' : ''}`;
+  } else if (horas > 0) {
+    return `hace ${horas} hora${horas > 1 ? 's' : ''}`;
+  } else if (minutos > 0) {
+    return `hace ${minutos} minuto${minutos > 1 ? 's' : ''}`;
+  } else {
+    return 'hace unos segundos';
+  }
+}
+
+
 export const exportToJson = (exportObj: Postulacion[]) => {
   const dataStr =
     "data:text/json;charset=utf-8," +
